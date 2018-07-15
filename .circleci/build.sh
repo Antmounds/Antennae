a55a8e39b10bcbdb293f0abc7ebb2fa8613879ff
Ant <nick@antmounds.com>
2018-07-14 21:54:07 -0600

    removed sensitive environment variables from build

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-IMAGE_NAME=$1
-AWS_IMAGE_NAME=$2
+
 # build meteor app if flag is set
 if [[ $* == --meteor ]]; then
 	# Build meteor app
@@ -11,13 +10,9 @@ if [[ $* == --meteor ]]; then
 fi
 
 # build the Docker image (this will use the Dockerfile in the root of the repo)
-docker build -t $IMAGE_NAME --build-arg BUILD=2468 --build-arg MONGO_URL=$MONGO_URL --build-arg AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID --build-arg AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY .
-exit;
-# authenticate with the AWS ECR registry
-$(aws ecr get-login --no-include-email --region us-east-1)
-# tag Docker image for the AWS ECR registry
-docker tag $IMAGE_NAME $AWS_IMAGE_NAME
-# push the new Docker image to the AWS ECR registry
-docker push $AWS_IMAGE_NAME
+docker build -t antennae --build-arg BUILD="dev-$(date '+%Y-%m-%d_%H:%M:%S')" --build-arg METEOR_SETTINGS="$(cat src/tools/settings.json)" .
+exit 0;
 
-docker run -d --rm -P --name ant --link mongo:mongo -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY antmounds/antennae
+
+
+# docker run -d --rm -P --name ant --link mongo:mongo -e MONGO_URL=$MONGO_URL -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY antmounds/antennae
